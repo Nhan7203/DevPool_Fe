23823649b1b494ab0ddf90f1a6d6730e84993fbe
chiemtantai <taictse172381@fpt.edu.vn>
2025-12-05 00:03:07 +0700

    fix

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,4 +1,4 @@
 // API Configuration - Single source of truth
 // Đọc từ biến môi trường .env, fallback về localhost nếu không có
-export const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:7298/api';
-
+export const API_URL = import.meta.env.VITE_API_URL || 'https://api-devpool.innosphere.io.vn/api' ;
+// 'https://localhost:7298/api'
